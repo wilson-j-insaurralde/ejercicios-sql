15a449d9ab90a1d4b7754a53fbecbd3dcbc6a761
wilson-j-insaurralde <wilson-insaurralde@hotmail.com>
2026-03-24 18:24:21 -0300

    ejercicio resuelto

--- 077_Restricciones foreign key (acciones)/03_ejercicio.sql
+++ 077_Restricciones foreign key (acciones)/03_ejercicio.sql
@@ -123,3 +123,82 @@ en "update_action".
 
 
 */
+
+if OBJECT_ID('inscripciones') is not null 
+drop table inscripciones ;
+if OBJECT_ID('deportes') is not null 
+drop table deportes ;
+if OBJECT_ID('socios') is not null 
+drop table socios;
+
+create table deportes(
+	codigo tinyint,
+	nombre varchar(20),
+	primary key(codigo)
+);
+create table socios(
+	documento char(8),
+	nombre varchar(30),
+	primary key (documento)
+);
+create table inscripciones(
+	documento char(8),
+	codigodeporte tinyint,
+	matricula char(1),-- 's' si está paga, 'n' si no está paga
+	primary key (documento,codigodeporte)
+);
+
+ alter table inscripciones
+  add constraint FK_inscripciones_codigodeporte
+  foreign key (codigodeporte)
+  references deportes(codigo)
+  on update cascade;
+
+ alter table inscripciones
+  add constraint FK_inscripciones_documento
+  foreign key (documento)
+  references socios(documento)
+  on delete cascade;
+
+ insert into deportes values(1,'basquet');
+ insert into deportes values(2,'futbol');
+ insert into deportes values(3,'natacion');
+ insert into deportes values(4,'tenis');
+
+ insert into socios values('30000111','Juan Lopez');
+ insert into socios values('31111222','Ana Garcia');
+ insert into socios values('32222333','Mario Molina');
+ insert into socios values('33333444','Julieta Herrero');
+
+ insert into inscripciones values ('30000111',1,'s');
+ insert into inscripciones values ('30000111',2,'s');
+ insert into inscripciones values ('31111222',1,'s');
+ insert into inscripciones values ('32222333',3,'n');
+
+ insert into inscripciones values('30000111',6,'s');
+
+ insert into inscripciones values('40111222',1,'s');
+
+ delete from deportes where nombre='tenis';
+
+ delete from deportes where nombre='natacion';
+
+ update deportes set codigo=5 where nombre='natacion';
+
+ select * from deportes;
+ select * from inscripciones;
+
+ delete from socios where documento='32222333'; 
+
+ select * from socios;
+ select * from inscripciones;
+
+ update socios set documento='35555555' where documento='30000111';
+
+ drop table deportes;
+
+ exec sp_helpconstraint socios;
+
+ exec sp_helpconstraint deportes;
+
+ exec sp_helpconstraint inscripciones;
