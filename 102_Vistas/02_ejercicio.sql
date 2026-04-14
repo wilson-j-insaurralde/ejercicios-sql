e3b9230bdce65cc7ccce2928c87e6a4e77789edf
wilson-j-insaurralde <wilson-insaurralde@hotmail.com>
2026-04-13 21:45:44 -0300

    echo

--- 102_Vistas/02_ejercicio.sql
+++ 102_Vistas/02_ejercicio.sql
@@ -109,3 +109,125 @@ inscriptos por curso, incluyendo el número del curso, el nombre del deporte y el
 
 */
 
+ if object_id('inscriptos') is not null  
+  drop table inscriptos;
+ if object_id('socios') is not null  
+  drop table socios;
+ if object_id('profesores') is not null  
+  drop table profesores; 
+ if object_id('cursos') is not null  
+  drop table cursos;
+
+ create table socios(
+  documento char(8) not null,
+  nombre varchar(40),
+  domicilio varchar(30),
+  constraint PK_socios_documento
+   primary key (documento)
+ );
+
+ create table profesores(
+  documento char(8) not null,
+  nombre varchar(40),
+  domicilio varchar(30),
+  constraint PK_profesores_documento
+   primary key (documento)
+ );
+
+ create table cursos(
+  numero tinyint identity,
+  deporte varchar(20),
+  dia varchar(15),
+   constraint CK_inscriptos_dia check (dia in('lunes','martes','miercoles','jueves','viernes','sabado')),
+  documentoprofesor char(8),
+  constraint PK_cursos_numero
+   primary key (numero),
+ );
+
+ create table inscriptos(
+  documentosocio char(8) not null,
+  numero tinyint not null,
+  matricula char(1),
+  constraint CK_inscriptos_matricula check (matricula in('s','n')),
+  constraint PK_inscriptos_documento_numero
+   primary key (documentosocio,numero)
+ );
+
+ insert into socios values('30000000','Fabian Fuentes','Caseros 987');
+ insert into socios values('31111111','Gaston Garcia','Guemes 65');
+ insert into socios values('32222222','Hector Huerta','Sucre 534');
+ insert into socios values('33333333','Ines Irala','Bulnes 345');
+
+ insert into profesores values('22222222','Ana Acosta','Avellaneda 231');
+ insert into profesores values('23333333','Carlos Caseres','Colon 245');
+ insert into profesores values('24444444','Daniel Duarte','Sarmiento 987');
+ insert into profesores values('25555555','Esteban Lopez','Sucre 1204');
+
+ insert into cursos values('tenis','lunes','22222222');
+ insert into cursos values('tenis','martes','22222222');
+ insert into cursos values('natacion','miercoles','22222222');
+ insert into cursos values('natacion','jueves','23333333');
+ insert into cursos values('natacion','viernes','23333333');
+ insert into cursos values('futbol','sabado','24444444');
+ insert into cursos values('futbol','lunes','24444444');
+ insert into cursos values('basquet','martes','24444444');
+
+ insert into inscriptos values('30000000',1,'s');
+ insert into inscriptos values('30000000',3,'n');
+ insert into inscriptos values('30000000',6,null);
+ insert into inscriptos values('31111111',1,'s');
+ insert into inscriptos values('31111111',4,'s');
+ insert into inscriptos values('32222222',8,'s');
+
+ if object_id('vista_club') is not null drop view vista_club;
+
+ create view vista_club as
+  select s.nombre as socio,s.documento as docsocio,s.domicilio as domsocio,c.deporte,dia,
+   p.nombre as profesor, matricula
+   from socios as s
+   full join inscriptos as i
+   on s.documento=i.documentosocio
+   full join cursos as c
+   on i.numero=c.numero
+   full join profesores as p
+   on c.documentoprofesor=p.documento;
+
+ select *from vista_club;
+
+ select deporte,dia,count(socio) as cantidad
+  from vista_club
+  where deporte is not null
+  group by deporte,dia
+  order by cantidad;
+
+ select deporte,dia from vista_club
+  where socio is null and deporte is not null;
+
+ select socio from vista_club
+  where deporte is null and socio is not null;
+
+ select profesor from vista_club where deporte is null and profesor is not null;
+
+ select socio, docsocio from vista_club where deporte is not null and matricula <> 's';
+
+ select distinct profesor,dia
+  from vista_club where profesor is not null;
+
+ select distinct profesor,dia
+  from vista_club where profesor is not null
+  order by dia;
+
+ select socio from vista_club
+  where deporte='tenis' and dia='lunes';
+
+ if object_id('vista_inscriptos') is not null
+  drop view vista_inscriptos;
+
+ create view vista_inscriptos as
+  select deporte,dia,
+   (select count(*)
+    from inscriptos as i
+    where i.numero=c.numero) as cantidad
+  from cursos as c;
+
+ select *from vista_inscriptos;
