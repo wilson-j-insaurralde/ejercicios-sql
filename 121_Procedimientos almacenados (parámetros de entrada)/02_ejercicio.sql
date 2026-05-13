a00467810aea43242d75886ee4fb87836b4c58a4
wilson-j-insaurralde <wilson-insaurralde@hotmail.com>
2026-05-13 16:45:24 -0300

    areglado mas teoria

--- 121_Procedimientos almacenados (parámetros de entrada)/02_ejercicio.sql
+++ 121_Procedimientos almacenados (parámetros de entrada)/02_ejercicio.sql
@@ -94,3 +94,60 @@ if object_id('empleados') is not null
  insert into empleados values ('22444444','Marta','Perez',500,1,'Sistemas');
  insert into empleados values('22555555','Susana','Garcia',400,2,'Secretaria');
  insert into empleados values('22666666','Jose Maria','Morales',400,3,'Secretaria');
+
+
+  if object_id('pa_empleados_sueldo') is not null
+  drop procedure pa_empleados_sueldo;
+
+  create procedure pa_empleados_sueldo
+  @sueldo decimal(6,2)
+  as 
+  select nombre,apellido, sueldo from empleados
+  where sueldo >= @sueldo;
+
+ exec pa_empleados_sueldo 400;
+ exec pa_empleados_sueldo 500;
+ 
+ exec pa_empleados_sueldo;
+
+ if OBJECT_ID('pa_empleados_actualizar_sueldo') is not null 
+ drop procedure pa_empleados_actualizar_sueldo ;
+
+ create procedure pa_empleados_actualizar_sueldo 
+   @sueldoanterior decimal(6,2),
+  @sueldonuevo decimal(6,2)
+ as 
+	update empleados set sueldo=@sueldonuevo
+	where sueldo=@sueldoanterior;
+
+ exec pa_empleados_actualizar_sueldo 300,350;
+ select * from empleados;
+
+  exec pa_empleados_actualizar_sueldo 350;
+
+ exec pa_empleados_actualizar_sueldo @sueldonuevo=400,@sueldoanterior=350;
+
+ select * from empleados;
+
+ if object_id('pa_sueldototal') is not null
+  drop procedure pa_sueldototal;
+
+  
+ create procedure pa_sueldototal
+  @documento varchar(8) = '%'
+ as
+  select nombre,apellido,
+   sueldototal=
+   case 
+    when sueldo<500 then sueldo+(cantidadhijos*200)
+    when sueldo>=500 then sueldo+(cantidadhijos*100)
+   end
+   from empleados
+   where documento like @documento;
+
+ exec pa_sueldototal '22333333';
+ exec pa_sueldototal '22444444';
+ exec pa_sueldototal '22666666';
+
+ exec pa_sueldototal;
+
