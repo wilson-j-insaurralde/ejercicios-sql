e4aeb10baa961218904d28035f2924c40d41e319
wilson-j-insaurralde <wilson-insaurralde@hotmail.com>
2026-06-05 09:50:14 -0300

    ejercicio

--- 134_Funciones escalares (crear y llamar)/02_ejercicio.sql
+++ 134_Funciones escalares (crear y llamar)/02_ejercicio.sql
@@ -74,3 +74,54 @@ función "f_nombreDia", la fecha (emplee la función "f_fecha"), el horario (emple
  select dbo.f_nombreDia(@valor);
 
 */
+
+if OBJECT_ID('consultas') is not null
+drop table  consultas;
+
+if OBJECT_ID('medicos') is not null 
+drop table medicos;
+
+create table medicos (
+  documento char(8) not null,
+  nombre varchar(30),
+  constraint PK_medicos 
+  primary key clustered (documento)
+  );
+
+
+ create table consultas(
+  fecha datetime,
+  medico char(8) not null,
+  paciente varchar(30),
+  constraint PK_consultas
+   primary key (fecha,medico),
+  constraint FK_consultas_medico
+   foreign key (medico)
+   references medicos(documento)
+   on update cascade
+   on delete cascade
+ );
+
+  insert into medicos values('22222222','Alfredo Acosta');
+ insert into medicos values('23333333','Pedro Perez');
+ insert into medicos values('24444444','Marcela Morales');
+
+ insert into consultas values('2007/03/26 8:00','22222222','Juan Juarez');
+ insert into consultas values('2007/03/26 8:00','23333333','Gaston Gomez');
+ insert into consultas values('2007/03/26 8:30','22222222','Nora Norte');
+ insert into consultas values('2007/03/28 9:00','22222222','Juan Juarez');
+ insert into consultas values('2007/03/29 8:00','24444444','Nora Norte');
+ insert into consultas values('2007/03/24 8:30','22222222','Hector Huerta'); 
+ insert into consultas values('2007/03/24 9:30','23333333','Hector Huerta');
+
+
+ if OBJECT_ID('f_nombreDia') is not null 
+ drop function f_nombreDia;
+
+ create function f_nombreDia 
+ (@fecha varchar(25))
+  returns varchar(25)
+  as 
+ begin 
+ declare 
+
