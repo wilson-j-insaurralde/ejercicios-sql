97a1aa3a0073694d5988eec60f067a90ac05d338
wilson-j-insaurralde <wilson-insaurralde@hotmail.com>
2026-07-08 18:40:23 -0300

    ejercicio:echo

--- 144_Disparadores (varios eventos)/02_ejercicio.sql
+++ 144_Disparadores (varios eventos)/02_ejercicio.sql
@@ -56,3 +56,58 @@ El trigger se ejecuta y la transacción se realiza.
 El trigger deshace la transacción.
 
 */
+ if object_id('empleados') is not null
+  drop table empleados;
+ if object_id('sucursales') is not null
+  drop table sucursales;
+
+ create table sucursales(
+  codigo int identity,
+  domicilio varchar(30),
+  constraint PK_sucursales primary key (codigo)
+ );
+
+ create table empleados(
+  documento char(8) not null,
+  nombre varchar(30),
+  domicilio varchar(30),
+  sucursal int not null,
+  constraint PK_empleados primary key (documento),
+  constraint FK_empleados_sucursal foreign key(sucursal)
+   references sucursales(codigo)
+ );
+
+ insert into sucursales values ('Colon 123');
+ insert into sucursales values ('Sucre 234');
+ insert into sucursales values ('Rivadavia 345');
+
+ insert into empleados values ('22222222','Ana Acosta','Avellaneda 1258',1);
+ insert into empleados values ('23333333','Betina Bustos','Bulnes 345',2);
+ insert into empleados values ('24444444','Carlos Caseres','Caseros 948',3);
+ insert into empleados values ('25555555','Fabian Fuentes','Francia 845',1);
+ insert into empleados values ('26666666','Gustavo Garcia','Guemes 587',2);
+ insert into empleados values ('27777777','Maria Morales','Maipu 643',3);
+
+ create trigger dis_empleados
+  on empleados
+  for insert,update,delete
+  as
+   declare @suc int
+   if (exists (select *from inserted where sucursal=1)) or
+     (exists (select *from deleted where sucursal=1))
+   begin
+    raiserror('No puede modificar datos de empleados de la sucursal 1', 16, 1)
+    rollback transaction
+   end;
+
+ insert into empleados values ('30000000','Zulma Zapata','Suiza 258',3);
+
+ insert into empleados values ('31111111','Ricardo Rojas','Rivadavia 256',1);
+
+ update empleados set domicilio='Avellaneda 234' where documento='23333333';
+
+ update empleados set domicilio='Avellaneda 234' where documento='22222222';
+
+ delete from empleados where documento='25555555';
+
+ delete from empleados where documento='30000000';
